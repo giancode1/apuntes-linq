5ec410ebafe96607e70ce7c243efddce5d4f2667
Giancarlo Culcay <gieg5c@gmail.com>
2023-01-01 23:54:28 -0500

    Inicio

---
+++ Book.cs
@@ -0,0 +1,16 @@
+// using System.Linq;
+
+public class Book
+{
+    // prop + tab
+    // ctor + tab
+    public string Title {get; set;}
+    public int PageCount {get; set;}
+    public string Status {get; set;}
+    public DateTime PublishedDate {get; set;}
+    // public string shortDescription {get; set;}
+    public string[] Authors {get; set;}
+    public string[] Categories {get; set;}
+
+    
+}
